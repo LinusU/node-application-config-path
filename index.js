ae49ff6748b68b29ec76c00ce4a28ba8e9161d9b
Feross Aboukhadijeh <feross@feross.org>
2019-07-30 04:20:56 -0700

    🌹 Prepare linting for Standard 14 (#1)

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ var os = require('os')
 var path = require('path')
 
 function darwin (name) {
-  return path.join(process.env['HOME'], 'Library', 'Application Support', name)
+  return path.join(process.env.HOME, 'Library', 'Application Support', name)
 }
 
 function linux (name) {
-  if (process.env['XDG_CONFIG_HOME']) {
-    return path.join(process.env['XDG_CONFIG_HOME'], name)
+  if (process.env.XDG_CONFIG_HOME) {
+    return path.join(process.env.XDG_CONFIG_HOME, name)
   }
 
-  return path.join(process.env['HOME'], '.config', name)
+  return path.join(process.env.HOME, '.config', name)
 }
 
 function win32 (name) {
-  if (process.env['LOCALAPPDATA']) {
-    return path.join(process.env['LOCALAPPDATA'], name)
+  if (process.env.LOCALAPPDATA) {
+    return path.join(process.env.LOCALAPPDATA, name)
   }
 
-  return path.join(process.env['USERPROFILE'], 'Local Settings', 'Application Data', name)
+  return path.join(process.env.USERPROFILE, 'Local Settings', 'Application Data', name)
 }
 
 function applicationConfigPath (name) {
